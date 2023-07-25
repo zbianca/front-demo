d5b4c155e1adeeb12a356bc90beed568b5948c58
Bianca Zanardi <bianca.zanardi@gmail.com>
2023-07-26 00:36:35 +0200

    Remove theme selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Card from "./Card.jsx";
 
 function App() {
-  const [theme, setTheme] = useState("light");
-
   return (
-    <main data-theme={theme} className="container">
-      <div className="header">
-        <h1>Cards Deck</h1>
-        <select
-          id="theme"
-          value={theme}
-          onChange={(e) => setTheme(e.target.value)}
-          aria-label="Theme"
-        >
-          <option value="light">light</option>
-          <option value="dark">dark</option>
-        </select>
-      </div>
+    <main data-theme="light" className="container">
+      <h1>Cards Deck</h1>
       {/* TODO: Add components */}
     </main>
   );
