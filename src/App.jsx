bbb48cc8308cbfa3b83fcc7e5803128f2ee424c2
Bianca Zanardi <bianca.zanardi@gmail.com>
2023-08-01 19:06:40 +0200

    Add Simplebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useId } from "react";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 import "./App.css";
 import Card from "./Card.jsx";
 
@@ -20,13 +22,15 @@ function App() {
     <main data-theme="light" className="container">
       <h1>Cards Deck</h1>
       <h2 id={deckId}>Our Silks</h2>
-      <ul className="deck" tabIndex={0} aria-labelledby={deckId}>
-        {cards.map((card) => (
-          <li>
-            <Card title={card.title} color={card.color} />
-          </li>
-        ))}
-      </ul>
+      <SimpleBar forceVisible="x" autoHide={false}>
+        <ul className="deck" aria-labelledby={deckId}>
+          {cards.map((card, i) => (
+            <li key={`${deckId}-${i}`}>
+              <Card title={card.title} color={card.color} />
+            </li>
+          ))}
+        </ul>
+      </SimpleBar>
     </main>
   );
 }
