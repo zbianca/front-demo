953fb52ed4238f45caffe6acf914637631654efb
Bianca Zanardi <bianca.zanardi@gmail.com>
2023-08-01 11:55:01 +0200

    Add content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
 import "./App.css";
 import Card from "./Card.jsx";
+
+const cards = [
+  { title: "Turquoise Silk", color: "turquoise" },
+  { title: "Teal Silk", color: "teal" },
+  { title: "Forest Green Silk", color: "forestgreen" },
+  { title: "Sea Green Silk", color: "seagreen" },
+  { title: "Coral Silk", color: "coral" },
+  { title: "Salmon Silk", color: "salmon" },
+  { title: "Crimson Silk", color: "crimson" },
+  { title: "Firebrick Silk", color: "firebrick" },
+];
 
 function App() {
   return (
     <main data-theme="light" className="container">
       <h1>Cards Deck</h1>
-      <Card title="Turquoise Silk" color="turquoise" />
+      {cards.map((card) => (
+        <Card title={card.title} color={card.color} />
+      ))}
     </main>
   );
 }
