7f9728a783b3b7e2c909e1c55527a0542aa7f419
Bianca Zanardi <bianca.zanardi@gmail.com>
2023-08-05 14:50:56 +0200

    Add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,46 @@ const cards = [
 
 function App() {
   const deckId = useId();
+  const sideScroll = (direction) => {
+    const container = document.querySelector(".simplebar-content-wrapper");
+    let scrollAmount = 0;
+
+    const slideTimer = setInterval(() => {
+      if (direction === "left") {
+        container.scrollLeft -= 20;
+      } else {
+        container.scrollLeft += 20;
+      }
+      scrollAmount += 10;
+      if (scrollAmount >= 100) {
+        window.clearInterval(slideTimer);
+      }
+    }, 25);
+  };
 
   return (
     <main data-theme="light" className="container">
       <h1>Cards Deck</h1>
       <h2 id={deckId}>Our Silks</h2>
-      <SimpleBar forceVisible="x" autoHide={false}>
-        <ul className="deck" aria-labelledby={deckId}>
-          {cards.map((card, i) => (
-            <li key={`${deckId}-${i}`}>
-              <Card title={card.title} color={card.color} />
-            </li>
-          ))}
-        </ul>
-      </SimpleBar>
+      <div className="scroll-wrapper">
+        <SimpleBar forceVisible="x" autoHide={false}>
+          <ul className="deck" aria-labelledby={deckId}>
+            {cards.map((card, i) => (
+              <li key={`${deckId}-${i}`}>
+                <Card title={card.title} color={card.color} />
+              </li>
+            ))}
+          </ul>
+        </SimpleBar>
+        <div className="buttons">
+          <button className="button-left" onClick={() => sideScroll("left")}>
+            ←
+          </button>
+          <button className="button-right" onClick={() => sideScroll("right")}>
+            →
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
